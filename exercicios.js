adef01587d3caa52057cab64a133f1dcce16f543
LUIS NUNES MARTINS <luis@prosol.com.br>
2016-07-22 09:29:07 -0300

    add exercícios 7, 8, 9 e 10

--- exercicios.js
+++ exercicios.js
@@ -93,3 +93,120 @@ function countChar(frase, caractere) {
 
 console.log(countBs("BBC"));
 console.log(countChar("kakkerlak","k"));
+
+// <-------------- Exercício 7 -------------->
+function range(start, end) {
+    var array = [];
+    
+    for (var index = start; index <= end; index++) {
+        array.push(index);
+    }
+
+    return array;
+}
+
+function sum(array) {
+    var soma = 0;
+    
+    for (var index = 0; index < array.length; index++) {
+        soma += array[index];
+    }
+
+    return soma;
+}
+
+var total = sum(range(1,10));
+
+console.log(total);
+
+// <-------------- Exercício 8 -------------->
+function reverseArray(array) {
+    var retorno = [];
+
+    for (var index = 0; index < array.length; index++) {
+        retorno.unshift(array[index]);
+    }
+
+    return retorno;
+}
+
+function reverseArrayInPlace(array) {
+    var auxiliar;
+
+    for (var index = 0; index < Math.floor(array.length / 2); index++) {
+        auxiliar = array[index];
+        array[index] = array[array.length - 1 - index];
+        array[array.length - 1 - index] = auxiliar;        
+    }
+
+    return array;
+}
+
+var array = [1,2,3];
+console.log(reverseArray(array));
+array = [1,2,3,4,5];
+console.log(reverseArrayInPlace(array));
+
+// <-------------- Exercício 9 -------------->
+function arrayToList(array) {
+  var list = null;
+  for (var i = array.length - 1; i >= 0; i--)
+    list = {value: array[i], rest: list};
+  return list;
+}
+
+function listToArray(list) {
+  var array = [];
+  for (var node = list; node; node = node.rest)
+    array.push(node.value);
+  return array;
+}
+
+function prepend(value, list) {
+  return {value: value, rest: list};
+}
+
+function nth(list, n) {
+  if (!list)
+    return undefined;
+  else if (n == 0)
+    return list.value;
+  else
+    return nth(list.rest, n - 1);
+}
+
+console.log(arrayToList([10, 20]));
+
+console.log(listToArray(arrayToList([10, 20, 30])));
+
+console.log(prepend(10, prepend(20, null)));
+
+console.log(nth(arrayToList([10, 20, 30]), 1));
+
+// <-------------- Exercício 10 -------------->
+function deepEqual(a, b) {
+  if (a === b) return true;
+  
+  if (a == null || typeof a != "object" ||
+      b == null || typeof b != "object")
+    return false;
+  
+  var propsInA = 0, propsInB = 0;
+
+  for (var prop in a)
+    propsInA += 1;
+
+  for (var prop in b) {
+    propsInB += 1;
+    if (!(prop in a) || !deepEqual(a[prop], b[prop]))
+      return false;
+  }
+
+  return propsInA == propsInB;
+}
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+console.log(deepEqual(obj, {here: 1, object: 2}));
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+
